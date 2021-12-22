ce29a129113eb2e71953dc052ee992e8349a85ab
Rh Proj <r.proj@hotmail.com>
2021-12-22 17:32:54 +0300

    Add project files.

---
+++ CodeBarProcAudit/Services/EPPlusService.cs
@@ -0,0 +1,75 @@
+﻿using OfficeOpenXml;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CodeBarProcAudit.Services
+{
+    public class EPPlusService
+    {
+        public static async Task<IEnumerable<List<string>>> LoadInventoryTable(FileInfo file)
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            List<List<string>> output = new();
+
+            using (var package = new ExcelPackage(file))
+            {
+                if (file.Exists)
+                {
+                    await package.LoadAsync(file);
+                }
+
+                var worksheet = package.Workbook.Worksheets[PositionID: 0];
+
+                int row = 2; int col = 1;
+                //читаем данные начиная с А2, пока есть данные(до конца таблицы)
+                while (string.IsNullOrWhiteSpace(worksheet.Cells[row, col].Value?.ToString()) == false) //если там есть значение - переведи ее в стринг
+                {
+                    List<string> item = new();
+                    while (string.IsNullOrWhiteSpace(worksheet.Cells[row, col].Value?.ToString()) == false)
+                    {
+                        item.Add(worksheet.Cells[row, col].Value.ToString());
+                        col++;
+                    }
+
+                    output.Add(item); //yield return item;
+                    row++;
+                }
+            }
+            return output;
+        }
+
+
+        //public static IEnumerable<string> GetDataLines(FileInfo file)
+        //{
+        //    using var data_stream = 
+        //    using var data_reader = new StreamReader();
+        //    //using var data
+        //    while (!data_reader.)
+        //    {
+
+        //    }
+        //}
+
+
+
+        public static async Task SaveToExcel(IEnumerable<string> data, FileInfo file)
+        {
+            using (var package = new ExcelPackage(file))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Инвентар");
+
+                var range = worksheet.Cells["A1"].LoadFromCollection(data, true);
+
+                range.AutoFitColumns();
+
+                worksheet.Row(1).Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+                worksheet.Row(1).Style.Font.Size = 20;
+                worksheet.Row(1).Style.Font.Bold = true;
+
+                await package.SaveAsync();
+            }
+        }
+    }
+}
---
+++ CodeBarProcAudit/ViewModels/MainViewModel.cs
@@ -0,0 +1,71 @@
+﻿using CodeBarProcAudit.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CodeBarProcAudit.ViewModels
+{
+    class MainViewModel //: INotifyPropertyChanged
+    {
+        //string path = $"{AppDomain.CurrentDomain.BaseDirectory}"; //\\Инвентарка
+        //public string excelFile;
+
+        private string _excelFile;
+        public string ExcelFile
+        {
+            get { return _excelFile; }
+            set { _excelFile = value; } // OnPropertyChanged(); 
+        }
+
+        private ObservableCollection<List<string>> _inventoryData;
+        public ObservableCollection<List<string>> InventoryData
+        {
+            get { return _inventoryData; }
+            set { _inventoryData = value; } // OnPropertyChanged();
+        }
+
+        public MainViewModel()
+        {
+            var table = GetExcelFile();
+
+            LoadData(table);
+        }
+
+        private async Task LoadData(FileInfo table)
+        {
+            InventoryData = new ObservableCollection<List<string>>(await EPPlusService.LoadInventoryTable(table));
+        }
+
+        FileInfo GetExcelFile()
+        {
+            string folderPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + (string)App.Current.Resources["InvFolder"];
+
+            ExcelFile = Directory.EnumerateFiles(folderPath).Where(s => s.EndsWith(".xlsx") || s.EndsWith(".xls")).Select(f => f).FirstOrDefault();
+
+            if (string.IsNullOrEmpty(ExcelFile))
+            {
+                //ExcelFile = "Отсутствует инвентарная таблица";
+                MessageBox.Show("Отсутствует инвентарная таблица");
+                return null;
+            }
+
+            FileInfo fileInf = new FileInfo(ExcelFile);
+            return fileInf;
+        }
+
+        //IEnumerable<string> InventoryName = await EPPlusService.LoadInventoryTable();
+
+
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //private void OnPropertyChanged([CallerMemberName] string propName = null)
+        //{
+        //    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        //}
+    }
+}
