5e76c4a3c80b0ed0fa432c1ee3c3201684c84989
Rh Proj <r.proj@hotmail.com>
2021-12-24 16:27:30 +0300

    SearchValueConverter

---
+++ CodeBarProcAudit/Converters/SearchValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace CodeBarProcAudit.Converters
+{
+    public class SearchValueConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string cellText = values[0] == null ? string.Empty : values[0].ToString();
+            string searchText = values[1] as string;
+
+            if (!string.IsNullOrEmpty(searchText) && !string.IsNullOrEmpty(cellText))
+            {
+                return cellText.ToLower().StartsWith(searchText.ToLower());
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
